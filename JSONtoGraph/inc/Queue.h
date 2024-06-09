7b9add3aad5ec2e75e817cb00de3d775dd7ebbba
NeoThePotato <neorayb@gmail.com>
2024-06-09 21:13:47 +0300

    Create Queue class.

---
+++ JSONtoGraph/inc/Queue.h
@@ -0,0 +1,39 @@
+#include "DynamicArray.h";
+
+#pragma once
+namespace Collections
+{
+	template <class T>
+	class Queue
+	{
+		DynamicArray<T>* _internalArray;
+
+	public:
+		Queue(size_t capacity = DEFAULT_CAPACITY)
+		{
+			_internalArray = new DynamicArray<T>(capacity);
+		}
+
+		~Queue()
+		{
+			delete _internalArray;
+		}
+
+		void Enqueue(T val)
+		{
+			_internalArray->Insert(val, 0);
+		}
+
+		T Dequeue()
+		{
+			T val = _internalArray->Get(_internalArray->Length() - 1);
+			_internalArray->Remove(_internalArray->Length() - 1);
+			return val;
+		}
+
+		size_t Length() const
+		{
+			return _internalArray->Length();
+		}
+	};
+}
